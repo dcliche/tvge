ccdfd5de362e24d311f5348d6f67c09620f6565a
Daniel Cliche <dcliche@meldora.com>
2021-07-16 15:29:52 -0400

    Added camera transform

--- camera.hpp
+++ camera.hpp
@@ -12,10 +12,16 @@ class Camera {
     void setOrthographicProjection(float left, float right, float top, float bottom, float near, float far);
     void setPerspectiveProjection(float fovy, float aspect, float near, float far);
 
+    void setViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up = glm::vec3{0.0f, -1.0f, 0.0f});
+    void setViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up = glm::vec3{0.0f, -1.0f, 0.0f});
+    void setViewYXZ(glm::vec3 position, glm::vec3 rotation);
+
     const glm::mat4& getProjection() const { return m_projectionMatrix; }
+    const glm::mat4& getView() const { return m_viewMatrix; }
 
    private:
     glm::mat4 m_projectionMatrix{1.0f};
+    glm::mat4 m_viewMatrix{1.0f};
 };
 
 }  // namespace tvge
